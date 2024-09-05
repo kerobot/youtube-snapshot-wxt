ae04401cff0507174315c6023eb5e1574cbb634e
Manabu Kikuchi <16217397+kerobot@users.noreply.github.com>
2024-09-05 11:08:15 +0900

    update.

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,3 +1,3 @@
 export default defineBackground(() => {
-  console.log('Hello background!', { id: browser.runtime.id });
+  console.log('Youtube Snapshot Extension background.', { id: browser.runtime.id });
 });
